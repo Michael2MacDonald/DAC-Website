a8a5ce6c3b9460105ccb50b0dbf5585897a615b3
Michael MacDonald <michael2macdonald@gmail.com>
2020-10-23 13:09:16 -0700

    fixed error in dbh.inc.php

--- include/dbh.inc.php
+++ include/dbh.inc.php
@@ -5,7 +5,7 @@ $dBPassword = "Muchnote2";
 $dBName = "dac";
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
-if(!conn){
+if(!$conn){
 	die("Connection Failed:".mysqli_connect_error());
 }
 ?>
